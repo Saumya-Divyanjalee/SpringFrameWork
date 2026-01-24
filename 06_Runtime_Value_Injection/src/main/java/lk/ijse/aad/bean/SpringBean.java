ae1a1bc12f3291d8d9c03d061d4ed7a341a318ba
saumy <saumyadivyanjalee529@gmail.com>
2026-01-24 14:43:08 +0530

    spring second-day

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/aad/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/aad/bean/SpringBean.java
@@ -1,23 +1,43 @@
 package lk.ijse.aad.bean;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean implements InitializingBean {
+public class SpringBean  {
+//    implements InitializingBean
 //    private String name = "SpringBean";
 
-@Value("saumya")
-private String name;
+//@Value("saumya")
+//private String name;
 
-    public SpringBean() {
+    //1 st step
+//    @Autowired(required = false)
+//    public SpringBean(@Value("saumya")String name,@Value("22")int age) {
+//        System.out.println(name);
+//        System.out.println(age);
+//    }
+
+
+    @Autowired(required = false)
+    public SpringBean(@Value("sau")String nameList[],@Value("12")int number,@Value("IJSE")String address) {
+        System.out.println(nameList.length);
+        System.out.println(number);
+        System.out.println(address);
+    }
+    //this constructor runing
+    @Autowired(required = false)
+    public SpringBean(@Value("saumya")String name,@Value("22")int age,@Value("sau")String name2) {
         System.out.println(name);
+        System.out.println(age);
+        System.out.println(name2);
     }
 
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println(name);
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println(name);
+//    }
 }
